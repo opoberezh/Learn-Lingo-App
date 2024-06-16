788a49b015fa4fa821610ad1db563943655cf15d
Olha Poberezhna <124092276+opoberezh@users.noreply.github.com>
2024-06-16 20:33:23 +0200

    teacher page

--- src/pages/TeachersPage/TeachersPage.jsx
+++ src/pages/TeachersPage/TeachersPage.jsx
@@ -1,5 +1,9 @@
 import  { useEffect } from 'react';
 import TeacherList from "../../components/TeacherList/TeacherList";
+import { Link } from 'react-router-dom';
+import ukraine from '../../../assets/ukraine.svg'
+
+
 
 const TeachersPage = () => {
   useEffect(() => {
@@ -14,6 +18,10 @@ const TeachersPage = () => {
 
   return (
     <div>
+<div>
+  <Link to='/'>
+  <img src={ukraine} alt="Ukraine Flag" /></Link>
+</div>
       <TeacherList />
     </div>
   );
