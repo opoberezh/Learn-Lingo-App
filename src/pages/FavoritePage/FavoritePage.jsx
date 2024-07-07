3eed16af3e9afbf19ca75c7b54c10c4bd6e92f3e
Olha Poberezhna <124092276+opoberezh@users.noreply.github.com>
2024-07-07 08:01:17 +0200

    favorite page styled

--- src/pages/FavoritePage/FavoritePage.jsx
+++ src/pages/FavoritePage/FavoritePage.jsx
@@ -2,9 +2,19 @@ import { useSelector } from "react-redux";
 import FavoriteList from "../../components/FavoriteList/FavoriteList";
 import { selectTotalFavorites } from "../../redux/favorites/selectors";
 import { Text, TextWrapper } from "./FavoritePage.styled";
+import { useEffect } from "react";
 
 const FavoritePage = () => {
   const totalFavorites = useSelector(selectTotalFavorites);
+
+  useEffect(() => {
+    document.body.style.backgroundColor = '#f0f0f0'; 
+
+    return () => {
+      document.body.style.backgroundColor = ''; 
+    };
+  }, []);
+
   return( 
   <div>
    {totalFavorites > 0 ? (<FavoriteList/>) : (<TextWrapper><Text>There are no favorites yet</Text></TextWrapper>)} 
